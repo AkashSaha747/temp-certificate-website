514c7072127b2d3d3460f4f9dc967bbde93c20fc
AkashSaha747 <akashkaliyaganj5@gmail.com>
2023-10-18 14:58:03 +0530

    fds

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://riteshshukla:riteshshukla@cluster0.fo6fefn.mongodb.net/digiblocks', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
